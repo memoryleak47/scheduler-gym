89757dfac1936dd30b5c6cebc87469c87096e75b
memoryleak47 <memoryleak47@protonmail.ch>
2026-07-21 17:03:14 +0200

    ...

--- dump.sh
+++ dump.sh
@@ -2,12 +2,15 @@
 
 for c in $(ls case-studies | sort -r)
 do
+    echo "=== $c:"
+
     for s in $(ls schedulers)
     do
         filename="benchdata/$s/$c.cost"
         [ ! -e "$filename" ] && continue
         score=$(cat "$filename")
-        echo "$s/$c = $score"
+        s2=$(printf "%-13s\n" "$s")
+        echo "$s2 = $score"
     done
 
     echo
